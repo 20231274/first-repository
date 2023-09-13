cf39970e025aa6cd348e0a0c1c0b42c0dde0b1ce
20231274 <bounce0926@gmail.com>
2023-09-13 15:39:31 +0900

    213

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 def print_hello():
-    print("Hello World!!")
+    print("Welcome to Korea!!")
 
 def print_KG():
-    print("아시안게임 롤 부문 한국 우승 화이팅")
+    print("한국인은 게임을 잘한다")
 
 if __name__ == '__main__':
     print_hello()
